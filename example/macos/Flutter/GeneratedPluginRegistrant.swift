59c573b33db11e8c894679e5c2c20ad9468a450f
SpaceXM <altairaerospace@gmail.com>
2024-06-15 15:35:47 +0200

    test

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import dart_vlc
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DartVlcPlugin.register(with: registry.registrar(forPlugin: "DartVlcPlugin"))
+}
