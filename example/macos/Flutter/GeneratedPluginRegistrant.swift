a2818cf9105e1ca6eefec5650f281b9e286deb55
SpaceXM <altairaerospace@gmail.com>
2024-06-20 00:06:19 +0200

    Make compiling again in Mac-Windows-Linux
    bumped version to 0.4.1

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import dart_vlc
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DartVlcPlugin.register(with: registry.registrar(forPlugin: "DartVlcPlugin"))
 }
